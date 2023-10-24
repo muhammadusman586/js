879b4d83a5c87ea2eac4b906a7a8e3178f622fcf
muhammadusman586 <146759960+muhammadusman586@users.noreply.github.com>
2023-10-24 14:06:16 +0000

    datatypes

---
+++ 0_1basics/0_1variables.js
@@ -0,0 +1,11 @@
+//console.log("Hello world")
+//console.log("I am Muhammad Usman ")
+
+const accountId=555555
+let accoutnEmail="usman9830142@gmail.com"
+var accountPassword=1112233
+
+// console.log(accountId,accoutnEmail,accountPassword)
+console.table([accountId,accoutnEmail,accountPassword])
+
+console.log(typeof "Usman")

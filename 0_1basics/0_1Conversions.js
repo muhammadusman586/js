9e3ff573146789108d7eb04baac7697a6c58eabd
muhammadusman586 <146759960+muhammadusman586@users.noreply.github.com>
2023-10-24 14:52:01 +0000

    Conver

---
+++ 0_1basics/0_1Conversions.js
@@ -0,0 +1,8 @@
+// let number="33"
+// let ConverIntoNumber=Number(number)
+// console.log(number)
+// console.log(typeof (ConverIntoNumber))
+// console.log(ConverIntoNumber)
+let x=5
+x--
+console.log(x)
